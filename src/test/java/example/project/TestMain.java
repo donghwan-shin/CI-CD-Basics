28480b32c708ae45ccd0975658767f308a2ca733
Donghwan Shin <d.shin@sheffield.ac.uk>
2023-03-12 21:00:15 +0000

    add example files and maven build config

---
+++ src/test/java/example/project/TestMain.java
@@ -0,0 +1,27 @@
+package example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+public class TestMain {
+
+    @Test
+    public void testHelloWorld()
+    {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+
+        // action
+        Main.main(null);
+
+        // assertion
+        assertEquals("Hello Reengineering!\n", bos.toString());
+
+        // undo the binding in System
+        System.setOut(originalOut);
+    }
+}
